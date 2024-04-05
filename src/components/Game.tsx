8b9c449131b2744688eb629ce6d16728f2d52b6e
Vignesh Moorthy <madhanvic@gmail.com>
2024-04-05 23:19:01 +0530

    Updates

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,13 +4,38 @@ import Board from "./Board";
 const Game = () =>{
     const [xIsNext, setXIsNext] = useState<boolean>(true);
     const[history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
     
-    const currentSquares = history[history.length - 1];
+    const currentSquares = history[currentMove];
 
     const handlePlay = (nextSquares:Array<null | 'X' | 'O'>) =>{
-           setHistory((prevState)=> [...prevState, nextSquares]);
+           const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+           setHistory(nextHistory);
+           setCurrentMove(nextHistory.length -1);
            setXIsNext((prevState)=>!prevState);
     }
+
+    const jumpTo = (nextMove:number)=>{
+           setCurrentMove(nextMove)
+           setXIsNext(nextMove % 2 === 0);
+    }
+
+    const moves = history.map((_, move)=>{
+        let description:string;
+        if(move > 0){
+            description = "Go to move#"+ move;
+        }else{
+            description = "Go to game start";
+        }
+
+        return (
+            <li key={move}>
+                <button onClick={jumpTo.bind(null, move)}>
+                    {description}
+                </button>
+            </li>
+        )
+    })
 
   return(
     <div className="game">
@@ -18,7 +43,7 @@ const Game = () =>{
             <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
         </div>
         <div className="game-info">
-            <ol></ol>
+            <ol>{moves}</ol>
         </div>
     </div>
   )
